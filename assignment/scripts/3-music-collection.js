67e1bb8f2f1b7771518ff90c94a1141d8e887441
Jeanee Allen <allenjeanee@yahoo.com>
2024-07-04 14:49:22 -0500

    created a showCollection that takes in an array, loops over it, and logs each item

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,17 +1,19 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
-const myCollection = [''];
+let myCollection = [];  // `myCollection` array initialized as an empty array
 
 function addToCollection(collection, title, artist, yearPublished) {
 let myAlbums = {
-title: title,
-artist: artist,
-yearPublished: yearPublished
+  collection: collection,
+  title: title,
+  artist: artist,
+  yearPublished: yearPublished
 };
+
 collection.push(myAlbums);
 return myAlbums
-}
+}                           // `addToCollection` pushes record object into the array & returns the object
 
 addToCollection(myCollection, "The Album About Nothing", "Wale", 2015);
 addToCollection(myCollection, "Ambition", "Wale", 2011);
@@ -20,11 +22,24 @@ addToCollection(myCollection, "The Miseducation of Lauryn Hill", "Lauryn Hill", 
 addToCollection(myCollection, "Illmatic", "Nas", 1994);
 addToCollection(myCollection, "Go for Your Guns","The Isley Brothers", 1977);
 
-console.log(myCollection)
+    console.log("my album", myCollection);   // Tested `addToCollection` by adding at least 6 albums & logged results
 
-
-
-
+function showCollection(collection) {
+  for (let items of collection) {
+    console.log(`${items.title} by ${items.artist}, published in ${items.yearPublished}`);
+  }
+}
+  let collection = [
+    {title: 'The Album About Nothing', artist: 'Wale', yearPublished: 2015},
+    {title: 'Ambition', artist: 'Wale', yearPublished: 2011},
+    {title: 'Traveller', artist: 'Chris Stapleton', yearPublished: 2015},
+    {title: 'The Miseducation of Lauryn Hill', artist: 'Lauryn Hill', yearPublished: 1998},
+    {title: 'Illmatic', artist: 'Nas', yearPublished: 1994},
+    {title: 'Go for Your Guns', artist: 'The Isley Brothers', yearPublished: 1977}
+  ]
+  console.log(collection);
+  showCollection(myCollection); // `showCollection` takes in an array, loops over it, correctly logs each item
+  
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
